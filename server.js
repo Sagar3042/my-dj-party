10dac67f33cc8c7d383f311d5d67eeeac01e8c09
Sagar3042 <113701975+Sagar3042@users.noreply.github.com>
2025-12-29 18:15:23 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -7,39 +7,39 @@ const io = new Server(server);
 
 app.get('/', (req, res) => { res.sendFile(__dirname + '/index.html'); });
 
-let studioState = {
-    videoId: "dQw4w9WgXcQ",
-    isPlaying: false,
-    adminTime: 0,
-    lastUpdate: Date.now()
-};
-
-let users = {};
+let users = {}; // Ekhane sob user er data thakbe
 
 io.on('connection', (socket) => {
     
-    // Join & Setup
-    socket.emit('init', { state: studioState, users: users });
-
+    // 1. User Join Korle
     socket.on('join', (data) => {
-        users[socket.id] = { name: data.name, role: data.role, time: 0 };
-        io.emit('update_users', users);
+        users[socket.id] = {
+            name: data.name,
+            role: data.role,
+            time: 0 // Prothome 0
+        };
+        // Sobai ke notun list pathao
+        io.emit('update_monitor', users);
     });
 
+    // 2. User Disconnect Hole
     socket.on('disconnect', () => {
         delete users[socket.id];
-        io.emit('update_users', users);
+        io.emit('update_monitor', users);
     });
 
-    // === REAL TIME SYNC ENGINE ===
-    
-    // 1. Admin sends heartbeat (Continuous Time Update)
+    // 3. USER REPORTING (User protteke second e tar time pathabe)
+    socket.on('report_time', (time) => {
+        if(users[socket.id]) {
+            users[socket.id].time = time;
+            // Admin ke update pathao
+            io.emit('update_monitor', users);
+        }
+    });
+
+    // 4. Admin Heartbeat (Admin er time shobaike pathano)
     socket.on('admin_heartbeat', (data) => {
-        studioState.isPlaying = data.isPlaying;
-        studioState.adminTime = data.time;
-        studioState.lastUpdate = Date.now();
-        
-        // Broadcast to all users immediately
+        // Broadcast to all users for sync
         socket.broadcast.emit('sync_pulse', {
             isPlaying: data.isPlaying,
             time: data.time,
@@ -47,30 +47,10 @@ io.on('connection', (socket) => {
         });
     });
 
-    // 2. Load Track
-    socket.on('change_track', (id) => {
-        studioState.videoId = id;
-        io.emit('load_track', id);
-    });
-
-    // 3. User Reports (For Monitor)
-    socket.on('report_time', (time) => {
-        if(users[socket.id]) {
-            users[socket.id].time = time;
-            socket.broadcast.emit('monitor_update', { id: socket.id, time: time });
-        }
-    });
-
-    // 4. Admin Force Controls
-    socket.on('admin_play', () => { io.emit('force_play'); });
-    socket.on('admin_pause', () => { io.emit('force_pause'); });
-    socket.on('admin_seek', (time) => { io.emit('force_seek', time); });
-
-    // 3D Audio
-    socket.on('admin_joystick', (coords) => {
-        socket.broadcast.emit('adjust_volume', coords); // Send to all to calc locally
-    });
+    // Standard Controls
+    socket.on('change_track', (id) => io.emit('load_track', id));
+    socket.on('admin_joystick', (coords) => socket.broadcast.emit('adjust_volume', coords));
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => { console.log("Instant Sync Server Running"); });
+server.listen(PORT, () => { console.log("Monitor Server Running"); });
